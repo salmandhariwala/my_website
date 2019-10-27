37f314dbdf8851b50a4169c3d2bc128330682496
salmandhariwala <salmandhariwala@gmail.com>
2019-10-28 00:40:11 +0530

    updated website

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
-aws s3 rm s3://salmandhariwala.com --recursive
-aws s3 sync --acl public-read /Users/salmandhariwala/Desktop/my_website/ s3://salmandhariwala.com
+#!/usr/bin/env bash
+aws s3 rm --profile personal s3://salmandhariwala.com --recursive
+aws s3 sync --profile personal --acl public-read . s3://salmandhariwala.com
+aws s3 rm --profile personal s3://salmandhariwala.com/.git --recursive
+aws s3 rm --profile personal s3://salmandhariwala.com/.idea --recursive
